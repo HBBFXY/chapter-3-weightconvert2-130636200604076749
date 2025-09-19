4f24c67b1029a20ada9e75db247fd712570588bc
130636200604076749 <jiafeimao1188@qq.com>
2025-09-19 10:13:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ for year in range(1, 11):
     earth_weight = initial_weight + 0.5 * year
     moon_weight = earth_weight * 0.165
     print(f"{year}\t{earth_weight:.2f}\t\t{moon_weight:.2f}")
-    
+if__name__=="__main__":
+   initial_weight=60
+calculate_weight(initial_weight)
