2ed8c8402ab2d798ede3c560a80d89d1eb475d30
130636200604076749 <jiafeimao1188@qq.com>
2025-09-19 10:14:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_weight(initial_weight):  
+initial_weight=60
 print("年份\t地球体重(kg)\t月球体重(kg)")
 for year in range(1, 11):
     earth_weight = initial_weight + 0.5 * year
     moon_weight = earth_weight * 0.165
     print(f"{year}\t{earth_weight:.2f}\t\t{moon_weight:.2f}")
-if__name__=="__main__":
-   initial_weight=60
-calculate_weight(initial_weight)
